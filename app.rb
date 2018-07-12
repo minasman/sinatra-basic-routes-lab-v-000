4b49d0bca744739d3a41f65f7ac59953fd84f269
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-07-12 16:02:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do
+  "Hello, World!"
+end
+
+get '/name' do 
+  "My name is Daniel"
+end
+
+get '/hometown' do
+  "My hometown is Las Cruces"
+end
+
+get '/favorite-song' do
+  "My favorite song is Hello"
+end
+  
 end
